2610e3454876dc8092960316d60219a7efb023b2
Sebastien <sebastien.guissart@prosehair.com>
2022-03-28 14:28:56 +0200

    feat: add check jupyter-notebook exectued in a row [sc-21769]

---
+++ tests/test_jupyter_check_notebook_cells_ran_in_a_row.py
@@ -0,0 +1,31 @@
+from testing.utils import get_resource_path
+import os
+import os.path as op
+import shutil
+import tempfile
+import filecmp
+import logging
+
+import pytest
+import difflib
+
+from decouple import config
+
+from prose_pre_commit_hooks.check_jupyter_notebook_cells_ran_in_a_row import main
+
+log_level = config('LOG_LEVEL', default='DEBUG')
+logging.basicConfig(level=log_level)
+
+
+@pytest.mark.parametrize(
+    ("filename", "expected_retval"),
+    (
+        ("bad_notebook.ipynb", 1),
+        ("good_notebook.ipynb", 0),
+        ("broken_notebook.ipynb", 1),
+    ),
+)
+def test_main(filename, expected_retval):
+    f_path = get_resource_path(filename)
+    ret = main([f_path])
+    assert ret == expected_retval
